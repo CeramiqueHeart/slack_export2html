c605edf72739a2f6bc7fe24f603d1797bc597881
CeramiqueHeart <CeramiqueHeart@users.noreply.github.com>
2022-08-22 14:33:00 +0900

    エントリのHashへの取り込みを実装

--- slack_export2html.rb
+++ slack_export2html.rb
@@ -1,10 +1,13 @@
 require 'zip'
 require 'tmpdir'
+require 'json'
 
 if $ARGV[0].nil?
     puts "Usage: " + $0 + " <Slack export file.zip>"
     exit
 end
+
+result = Hash.new
 
 begin
     Dir.mktmpdir do |tmpdir|
@@ -12,6 +15,18 @@ begin
             zip.each do |entry|
                 p entry.name
                 zip.extract(entry, tmpdir + entry.name) { true }
+
+                # ディレクトリは無視する
+                if entry.name.slice(-1) == "/"
+                    next
+                end
+
+                file = File.open(tmpdir + entry.name, "r")
+                contents = JSON.parse(file.read)
+                
+                contents.each do |item|
+                    result[entry.name] = [item["text"], item["ts"]]
+                end
             end
         end
     end
@@ -19,3 +34,6 @@ rescue
     puts "Cannot open " + $ARGV[0]
 end
 
+result.sort.each do |item|
+    p item
+end
